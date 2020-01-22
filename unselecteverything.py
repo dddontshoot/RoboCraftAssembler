96f2206f8e07a84ba459ed4c3b8baae94f890eb1
dddontshoot <32731888+dddontshoot@users.noreply.github.com>
2020-01-23 00:43:27 +1300

    Update unselecteverything.py

--- unselecteverything.py
+++ unselecteverything.py
@@ -4,12 +4,3 @@ def go():
     if len(selected) > 0:                 # I want to un-select everything
                 for obj in selected:
                     obj.select=False
-
-def deletethecube():
-    import bpy
-    if not bpy.context.object == None:
-        print("found some trash")
-        if bpy.context.object.name=="Cube":
-            print("cleaning up some trash")
-            bpy.ops.object.delete(use_global=True)
-            print("existing object:",bpy.context.object.name)

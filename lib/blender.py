be75aa5d1b858d0fc9d2986d36074d56feff3c74
dddontshoot <32731888+dddontshoot@users.noreply.github.com>
2020-02-23 15:07:39 +1300

    Fixed ColourOveride bug
    Colour Overide now applies to only relevant objects, not all object in the cube.
    The colour table now references the variable colourOveride, and not cube["Colour"] directly.

--- lib/blender.py
+++ lib/blender.py
@@ -45,9 +45,10 @@ def build(cubeDataHex, colourDataHex, cubeCount, cubedatabase):
         
 
         for filename in objectlist: 
+            colourOveride=cube["Colour"]
             if "ColourOveride" in filename: 
-                    filename, rubbish, cube["Colour"] = filename.split("=") 
-                    cube["Colour"] = int(cube["Colour"])
+                    filename, rubbish, colourOveride = filename.split("=") 
+                    colourOveride = int(colourOveride)
             datum = cube["name"] + "." + filename 
             
             if datum not in cubesinuse:  
@@ -158,42 +159,42 @@ def build(cubeDataHex, colourDataHex, cubeCount, cubedatabase):
                     bpy.ops.transform.rotate(value=1.5708, axis=(0, 1, 0), constraint_axis=(False, True, False), constraint_orientation='GLOBAL', mirror=False, proportional='ENABLED', proportional_edit_falloff='SMOOTH', proportional_size=1)
 
 
-                textureandcolour=str(datum)+"."+str(cube["Colour"]) 
+                textureandcolour=str(datum)+"."+str(colourOveride) 
                 if textureandcolour not in coloursinuse: 
 
                         newMaterial=bpy.data.objects[datum].active_material.copy()
-                        if cube["Colour"]== 20 : newMaterial.diffuse_color=(1.0 ,0.683 ,0.107)     # Birch
-                        if cube["Colour"]== 4  : newMaterial.diffuse_color=(0.02 ,0.02 ,0.02)      # Black
-                        if cube["Colour"]== 23 : newMaterial.diffuse_color=(0.055 ,0.052 ,0.077)   # Blue01
-                        if cube["Colour"]== 24 : newMaterial.diffuse_color=(0.029 ,0.13 ,0.199)    # Blue02
-                        if cube["Colour"]== 25 : newMaterial.diffuse_color=(0.139 ,0.33 ,0.761)    # Blue03
-                        if cube["Colour"]== 26 : newMaterial.diffuse_color=(0.202 ,0.234 ,0.296)   # Blue04
-                        if cube["Colour"]== 27 : newMaterial.diffuse_color=(0.36 ,0.433 ,0.646)    # Blue05
-                        if cube["Colour"]== 28 : newMaterial.diffuse_color=(0.406 ,0.877 ,1.0)     # Blue06
-                        if cube["Colour"]== 29 : newMaterial.diffuse_color=(0.134 ,0.071 ,0.233)   # Blue07
-                        if cube["Colour"]== 8  : newMaterial.diffuse_color=(1.0 ,0.0 ,0.51)        # Bright_Purple
-                        if cube["Colour"]== 11 : newMaterial.diffuse_color=(0.416 ,0.098 ,0.012)   # Brown
-                        if cube["Colour"]== 9  : newMaterial.diffuse_color=(0.068 ,0.028 ,0.834)   # Dark_Blue
-                        if cube["Colour"]== 17 : newMaterial.diffuse_color=(0.528 ,0.059 ,0.038)   # Dark_Brown
-                        if cube["Colour"]== 21 : newMaterial.diffuse_color=(0.0 ,0.136 ,0.044)     # Dark_Green
-                        if cube["Colour"]== 22 : newMaterial.diffuse_color=(0.097 ,0.108 ,0.016)   # Dark_Olive
-                        if cube["Colour"]== 7  : newMaterial.diffuse_color=(0.046 ,0.679 ,0.033)   # Green
-                        if cube["Colour"]== 1  : newMaterial.diffuse_color=(0.288 ,0.288 ,0.288)   # Grey
-                        if cube["Colour"]== 3  : newMaterial.diffuse_color=(0.0 ,0.748 ,0.8)       # Light_Blue
-                        if cube["Colour"]== 13 : newMaterial.diffuse_color=(0.086 ,0.319 ,0.074)   # Light_Green
-                        if cube["Colour"]== 12 : newMaterial.diffuse_color=(0.603 ,0.555 ,0.047)   # Light_Olive
-                        if cube["Colour"]== 30 : newMaterial.diffuse_color=(0.828 ,0.077 ,0.81)    # Light_Purple
-                        if cube["Colour"]== 16 : newMaterial.diffuse_color=(0.883 ,0.03 ,0.002)    # Light_Red
-                        if cube["Colour"]== 2  : newMaterial.diffuse_color=(1.0 ,0.282 ,0.0)       # Orange
-                        if cube["Colour"]== 15 : newMaterial.diffuse_color=(1.0 ,0.412 ,0.412)     # Pale_Pink
-                        if cube["Colour"]== 31 : newMaterial.diffuse_color=(0.757 ,0.518 ,0.846)   # Pale_Purple
-                        if cube["Colour"]== 14 : newMaterial.diffuse_color=(1.0 ,0.804 ,0.38)      # Pale_Yellow
-                        if cube["Colour"]== 19 : newMaterial.diffuse_color=(0.321 ,0.229 ,0.229)   # Pink
-                        if cube["Colour"]== 18 : newMaterial.diffuse_color=(0.367 ,0.175 ,0.211)   # Pinky_Brewster
-                        if cube["Colour"]== 10 : newMaterial.diffuse_color=(0.376 ,0.0 ,0.336)     # Purple
-                        if cube["Colour"]== 5  : newMaterial.diffuse_color=(0.448 ,0.008 ,0.007)   # Red
-                        if cube["Colour"]== 0  : newMaterial.diffuse_color=(1.0 ,1.0 ,1.0)         # White
-                        if cube["Colour"]== 6  : newMaterial.diffuse_color=(0.992 ,0.867 ,0.098)   # Yellow
+                        if colourOveride== 20 : newMaterial.diffuse_color=(1.0 ,0.683 ,0.107)     # Birch
+                        if colourOveride== 4  : newMaterial.diffuse_color=(0.02 ,0.02 ,0.02)      # Black
+                        if colourOveride== 23 : newMaterial.diffuse_color=(0.055 ,0.052 ,0.077)   # Blue01
+                        if colourOveride== 24 : newMaterial.diffuse_color=(0.029 ,0.13 ,0.199)    # Blue02
+                        if colourOveride== 25 : newMaterial.diffuse_color=(0.139 ,0.33 ,0.761)    # Blue03
+                        if colourOveride== 26 : newMaterial.diffuse_color=(0.202 ,0.234 ,0.296)   # Blue04
+                        if colourOveride== 27 : newMaterial.diffuse_color=(0.36 ,0.433 ,0.646)    # Blue05
+                        if colourOveride== 28 : newMaterial.diffuse_color=(0.406 ,0.877 ,1.0)     # Blue06
+                        if colourOveride== 29 : newMaterial.diffuse_color=(0.134 ,0.071 ,0.233)   # Blue07
+                        if colourOveride== 8  : newMaterial.diffuse_color=(1.0 ,0.0 ,0.51)        # Bright_Purple
+                        if colourOveride== 11 : newMaterial.diffuse_color=(0.416 ,0.098 ,0.012)   # Brown
+                        if colourOveride== 9  : newMaterial.diffuse_color=(0.068 ,0.028 ,0.834)   # Dark_Blue
+                        if colourOveride== 17 : newMaterial.diffuse_color=(0.528 ,0.059 ,0.038)   # Dark_Brown
+                        if colourOveride== 21 : newMaterial.diffuse_color=(0.0 ,0.136 ,0.044)     # Dark_Green
+                        if colourOveride== 22 : newMaterial.diffuse_color=(0.097 ,0.108 ,0.016)   # Dark_Olive
+                        if colourOveride== 7  : newMaterial.diffuse_color=(0.046 ,0.679 ,0.033)   # Green
+                        if colourOveride== 1  : newMaterial.diffuse_color=(0.288 ,0.288 ,0.288)   # Grey
+                        if colourOveride== 3  : newMaterial.diffuse_color=(0.0 ,0.748 ,0.8)       # Light_Blue
+                        if colourOveride== 13 : newMaterial.diffuse_color=(0.086 ,0.319 ,0.074)   # Light_Green
+                        if colourOveride== 12 : newMaterial.diffuse_color=(0.603 ,0.555 ,0.047)   # Light_Olive
+                        if colourOveride== 30 : newMaterial.diffuse_color=(0.828 ,0.077 ,0.81)    # Light_Purple
+                        if colourOveride== 16 : newMaterial.diffuse_color=(0.883 ,0.03 ,0.002)    # Light_Red
+                        if colourOveride== 2  : newMaterial.diffuse_color=(1.0 ,0.282 ,0.0)       # Orange
+                        if colourOveride== 15 : newMaterial.diffuse_color=(1.0 ,0.412 ,0.412)     # Pale_Pink
+                        if colourOveride== 31 : newMaterial.diffuse_color=(0.757 ,0.518 ,0.846)   # Pale_Purple
+                        if colourOveride== 14 : newMaterial.diffuse_color=(1.0 ,0.804 ,0.38)      # Pale_Yellow
+                        if colourOveride== 19 : newMaterial.diffuse_color=(0.321 ,0.229 ,0.229)   # Pink
+                        if colourOveride== 18 : newMaterial.diffuse_color=(0.367 ,0.175 ,0.211)   # Pinky_Brewster
+                        if colourOveride== 10 : newMaterial.diffuse_color=(0.376 ,0.0 ,0.336)     # Purple
+                        if colourOveride== 5  : newMaterial.diffuse_color=(0.448 ,0.008 ,0.007)   # Red
+                        if colourOveride== 0  : newMaterial.diffuse_color=(1.0 ,1.0 ,1.0)         # White
+                        if colourOveride== 6  : newMaterial.diffuse_color=(0.992 ,0.867 ,0.098)   # Yellow
                         newMaterial.specular_color=newMaterial.diffuse_color              # make the specular colour the same as the diffuse colour
                         newMaterial.name=textureandcolour                         
                         coloursinuse.append(textureandcolour)
